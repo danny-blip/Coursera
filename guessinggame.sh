7414ec0be4d0ac6e86b1614aece0e3d4542ac9d3
danny-blip <58196602+danny-blip@users.noreply.github.com>
2020-12-13 23:42:33 -0600

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,27 +1,27 @@
-echo "How many files are in this directory"
+echo "How many files are in this directory?"
 read guess
 if [ $guess -eq 10 ]
 then
-        echo "Congrats you win"
+        echo "Congrats, you win!"
 else
-        echo "Try again"
+        echo "Try again..."
         while [ $guess -ne 10 ]
         do
-                echo "How many files are in this directory"
+                echo "How many files are in this directory?"
                 read guess
                 if [ $guess -gt 10 ]
                 then
-                        echo "Too far"
+                        echo "Too far!"
                 else
                         if [ $guess -gt 10 ]
                         then
-                                echo "Too  far"
+                                echo "Too  far!"
                         else
                                 if [ $guess  -lt 10 ]
                                 then
-                                        echo "Too near"
+                                        echo "Too near!"
                                 else
-                                        echo "Congrats you win"
+                                        echo "Congrats, you win!"
                                 fi
                         fi
                 fi
